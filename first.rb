3c3506764e64b54f4d7b3f013491063f91f79810
vicki-mahajan-13 <mahajanvickyaa78@gmail.com>
2022-11-05 11:24:58 +0530

    First Ruby Program

---
+++ first.rb
@@ -0,0 +1,23 @@
+require 'byebug'
+a = [1,2,34,4,5,6,7,8,9]
+
+class Integer
+    def prime?
+       return false if self < 2
+
+        (2..(self/2)).each do |num|
+            if self % num == 0
+                return false
+            end
+            return true
+        end
+    end
+
+    # def even?
+    #   self % 2 != 0
+    # end
+
+    alias :sam? :even?
+end
+
+puts a.select(&:sam?)
