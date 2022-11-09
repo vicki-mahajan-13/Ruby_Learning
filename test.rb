aa950cf61e04f010fd653568b50a8c05e7a87f92
vicki-mahajan-13 <mahajanvickyaa78@gmail.com>
2022-11-09 15:22:07 +0530

    don't give me five

---
+++ test.rb
@@ -0,0 +1,16 @@
+def dont_give_me_five(start_,end_)
+  arr=[]
+  (start_..end_).each { |n| arr << n if n/10==5||n%10==5 }
+  return ((start_..end_).count)-arr.length
+end
+
+puts dont_give_me_five(57,66)
+
+
+
+
+
+=begin
+  Test.assert_equals(dont_give_me_five(1,9), 8)
+  Test.assert_equals(dont_give_me_five(4,17), 12)
+=end
