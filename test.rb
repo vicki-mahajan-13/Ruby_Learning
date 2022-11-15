0a26988533b54f8cf2b49e5291e57d502c95bae6
vicki-mahajan-13 <mahajanvickyaa78@gmail.com>
2022-11-15 11:46:42 +0530

    don't give me five

--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 def dont_give_me_five(start_,end_)
   arr=[]
   (start_..end_).each { |n| arr << n if n/10==5||n%10==5 }
-  return ((start_..end_).count)-arr.length
+  ((start_..end_).count)-arr.length
 end
 
 puts dont_give_me_five(57,66)
