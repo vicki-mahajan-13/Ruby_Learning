a0086edad85533a367d8335c16dee930c2d63470
vicki-mahajan-13 <mahajanvickyaa78@gmail.com>
2022-11-16 18:03:16 +0530

    bmi calculation with the help of weight and height

---
+++ bmi_calculation.rb
@@ -0,0 +1,25 @@
+def bmi(weight, height)
+  bmi = weight/(height**2)
+  if bmi<=18.5
+    return "Underweight"
+  elsif bmi<=25.0
+    return "Normal"
+  elsif bmi<=30.0
+    return "Overweight"
+  else
+    return "Obese"
+  end
+end
+
+puts bmi(80,1.80)
+
+
+
+
+=begin
+    Test.assert_equals(bmi(50, 1.80), "Underweight")
+    Test.assert_equals(bmi(80, 1.80), "Normal")
+    Test.assert_equals(bmi(90, 1.80), "Overweight")
+    Test.assert_equals(bmi(110, 1.80), "Obese")
+    Test.assert_equals(bmi(50, 1.50), "Normal")
+=end
