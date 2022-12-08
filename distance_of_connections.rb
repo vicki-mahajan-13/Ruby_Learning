0211a51e8fdeec9f8b83790aeac487b6d759c551
vicki-mahajan-13 <mahajanvickyaa78@gmail.com>
2022-12-08 19:48:15 +0530

    return the distance between the given two names from an array of connections

---
+++ distance_of_connections.rb
@@ -0,0 +1,33 @@
+def distance(connections, name1, name2)
+    b=ele_pair(connections).index(name2+name2)
+    a=ele_pair(connections).index(name1+name1)
+    if a>b
+       return a-b
+    else
+        return b-a
+    end
+end
+
+def ele_pair(arr)
+    i=0
+    str1=[]
+    str=seperate(arr)
+    while i<str.length
+        str1<<str[i]+str[i]
+        i+=1
+    end
+    return str1
+end
+
+def seperate(connections)
+    i=0
+    arr=[]
+    while i<connections.length
+       arr << connections[i].split(':')
+       i+=1
+    end
+    return arr.flatten.uniq
+end
+
+
+print distance(["vicky:shivam", "shivam:mahesh", "mahesh:prasoon", "prasoon:tinku", "shivam:tinku"], "tinku", "vicky")
