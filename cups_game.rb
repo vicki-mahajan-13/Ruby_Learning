7738b86d62ba274b3c14133c6e99262d63f6fc27
vicki-mahajan-13 <mahajanvickyaa78@gmail.com>
2022-12-19 18:53:15 +0530

    update_cups_game

--- cups_game.rb
+++ cups_game.rb
@@ -1,18 +1,21 @@
+
 def cup_and_balls(b, arr)
   i=0
   cups=[1,2,3]
-  arr=arr.flatten
   length=arr.length
   while i<length
-    cups[arr[i]-1], cups[arr[i+1]-1] = cups[arr[i+1]-1], cups[arr[i]-1]
-    i+=2
+    cups[arr[i][0]-1], cups[arr[i][1]-1] = cups[arr[i][1]-1], cups[arr[i][0]-1]
+    puts cups
+    i+=1
   end
   cups.find_index(b)+1
 end
 
-print cup_and_balls(2,[[1,2]])
+puts cup_and_balls(2,[[1,2], [2,1], [1,2], [1,3], [1,2]])
 
 
+# [[1,2],3,4]
+# arr[0][1]
 =begin
     Test.assert_equals(cup_and_balls(2,[[1,2]]), 1)
     Test.assert_equals(cup_and_balls(1,[[2,3],[1,2],[1,2]]), 1)
